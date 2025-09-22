feb762560ff26fcba948e763d0c0e15135c56bd5
Simon Hoyos <shmesa23@gmail.com>
2025-09-22 18:37:01 -0500

    implement classNames merge function

---
+++ src/classNames.ts
@@ -0,0 +1,39 @@
+export type ClassValue =
+  | ClassArray
+  | ClassDictionary
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
+export type ClassDictionary = Record<string, unknown>;
+export type ClassArray = Array<ClassValue>;
+
+function convertObjectToString(obj: ClassDictionary) {
+  return Object.entries(obj)
+    .filter(([, value]) => Boolean(value) === true)
+    .map(([key]) => `${key}`)
+    .join(' ');
+}
+
+export default function classNames(...args: Array<ClassValue>): string {
+  if (args.length <= 0) return '';
+
+  return args
+    .filter((value) => value != null && Boolean(value) === true)
+    .map((value) => {
+      if (value == null || typeof value === 'boolean') return '';
+
+      if (typeof value === 'string' || typeof value === 'number') {
+        return value;
+      }
+
+      if (Array.isArray(value)) {
+        return classNames(...value);
+      }
+
+      return convertObjectToString(value);
+    })
+    .join(' ')
+    .trim();
+}
